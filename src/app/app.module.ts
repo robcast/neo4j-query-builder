57ea572c095f750b1277150cc910145ed4d33a11
Robert Casties <casties@mpiwg-berlin.mpg.de>
2018-08-27 18:57:40 +0200

    First try using Material DataTable. Doesn't work yet.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { QueryComponent } from './query/query.component';
@@ -9,20 +14,24 @@ import { SelectComponent } from './select/select.component';
 import { ResultTableComponent } from './result-table/result-table.component';
 import { ResultRowComponent } from './result-table/result-row.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    QueryComponent,
-    SelectComponent,
-    ResultTableComponent,
-    ResultRowComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule( {
+    declarations: [
+        AppComponent,
+        QueryComponent,
+        SelectComponent,
+        ResultTableComponent,
+        ResultRowComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatTableModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+} )
 export class AppModule { }
---
+++ src/app/result-table-html/result-table.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { QueryState } from '../model/query-state';
+import { ResultRowComponent } from './result-row.component';
+
+
+@Component( {
+    selector: 'query-result',
+    templateUrl: './result-table.component.html',
+    styleUrls: ['./result-table.component.css'],
+    inputs: ['queryState', 'resultInfo']
+} )
+export class ResultTableComponent {
+
+    public queryState: QueryState;
+    public resultInfo: string;
+
+}
